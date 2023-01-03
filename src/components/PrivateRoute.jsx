62f8cc331598736e964a0da9b412f5bb1026eab0
Oleksandr Chyhryn <as.chigrin@gmail.com>
2023-01-03 22:31:34 +0200

    adding propTypes

--- src/components/PrivateRoute.jsx
+++ src/components/PrivateRoute.jsx
@@ -1,6 +1,7 @@
 import { useSelector } from 'react-redux';
 import { Navigate } from 'react-router-dom';
 import { selectAuthState } from 'redux/auth/authSelectors';
+import PropTypes from 'prop-types';
 
 export const PrivateRoute = ({
   component: Component,
@@ -9,4 +10,9 @@ export const PrivateRoute = ({
   const { isLoggedIn, isRefreshing } = useSelector(selectAuthState);
   const shouldRedirect = !isRefreshing && !isLoggedIn;
   return shouldRedirect ? <Navigate to={redirectTo} /> : <Component />;
+};
+
+PrivateRoute.propTypes = {
+  component: PropTypes.func.isRequired,
+  redirectTo: PropTypes.string,
 };
--- src/components/RestrictedRoute.jsx
+++ src/components/RestrictedRoute.jsx
@@ -1,8 +1,14 @@
 import { useSelector } from 'react-redux';
 import { Navigate } from 'react-router-dom';
 import { selectAuthState } from 'redux/auth/authSelectors';
+import PropTypes from 'prop-types';
 
 export const RestrictedRoute = ({ component: Component, redirectTo = '/' }) => {
   const { isLoggedIn } = useSelector(selectAuthState);
   return isLoggedIn ? <Navigate to={redirectTo} /> : <Component />;
+};
+
+RestrictedRoute.propTypes = {
+  component: PropTypes.func.isRequired,
+  redirectTo: PropTypes.string,
 };
